80933f69f3a45782b93434b608bee2e8465b98e3
j2usthack <dimas.mihailov@gmail.com>
2022-10-15 09:47:13 +0700

    Готовая программа проверки на високосный год

---
+++ Leap year/Leap year/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap_year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите год для проверки на високосность: ");
+            string userString = Console.ReadLine();
+            int n, t;            
+            n = Convert.ToInt32(userString);                     
+            t = n % 4;
+            if (t == 0)
+                Console.Write("Год високосный");
+            else
+                Console.Write("Год НЕ високосный");
+            
+        }
+    }
+}
